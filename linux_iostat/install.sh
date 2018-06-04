a58de97856d27f1e40c1523232a68bbd6ed64768
root <bq@bissquit.com>
2018-06-04 17:54:48 +0300

    last changes

--- linux_iostat/install.sh
+++ linux_iostat/install.sh
@@ -8,9 +8,9 @@
 #               Install and assign template to your host at Zabbix Server
 #               before script execution
 #
-#        NOTES: the scipt has optimized for Debian 9.1.
+#        NOTES: the scipt has optimized for Debian 9.
 #       AUTHOR: E.S.Vasilyev - bq@bissquit.com; e.s.vasilyev@mail.ru
-#      VERSION: 1.0.1
+#      VERSION: 1.1.0
 #      CREATED: 04.06.2018
 #=========================================================================
 
@@ -28,7 +28,7 @@ script_log_path="/tmp/linux_iostat_install.sh.log"
 exec >/dev/null 2> "$script_log_path"
 
 [[ -z $( sed '/^Include=/!d' "$zabbix_conf_path" ) ]] && \
-	echo 'Error!!! Define Include= option in zabbixs alert config and run install.sh again. Exit...'; exit 1
+        { echo 'Error!!! Define Include= option in zabbixs alert config and run install.sh again. Exit...'; exit 1; }
 
 #-------------------------------------------------------------------------
 # sysstat installation ( is mandatory )
@@ -39,13 +39,19 @@ apt-get install -y sysstat
 # zabbix agent config changing
 #-------------------------------------------------------------------------
 [[ -z $(sed '/^Timeout=.*$/!d' "$zabbix_conf_path") ]] && \
-	echo 'Timeout=30' >> "$zabbix_conf_path"
+        echo 'Timeout=30' >> "$zabbix_conf_path"
 
+# clean old data
+rm ${zabbix_ext_conf_path}/iostat*
+
+# download files
 wget https://raw.githubusercontent.com/bissquit/zabbix/master/linux_iostat/iostat.conf -P "${zabbix_ext_conf_path}/"
 wget https://raw.githubusercontent.com/bissquit/zabbix/master/linux_iostat/iostat.data.sh -P "${zabbix_ext_conf_path}/"
 
-chmod a+x "${zabbix_ext_conf_path}/iostat.data.sh"
+# replace path
+# this is bad idea to use sed as solution. I will think how to replace path more safely
+sed -i "s|/path/to/iostat.data.sh|$zabbix_ext_conf_path/iostat\.data\.sh|" "${zabbix_ext_conf_path}/iostat.conf"
 
-echo "UserParameter=iostat.data[*],${zabbix_ext_conf_path}/iostat.data.sh /tmp/iostat.data.output $1 $2" >> "${zabbix_ext_conf_path}/iostat.conf"
+chmod a+x "${zabbix_ext_conf_path}/iostat.data.sh"
 
 service zabbix-agent restart
